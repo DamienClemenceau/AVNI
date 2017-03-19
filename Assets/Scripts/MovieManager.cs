4d6b694dc3feb0b9c8acdd21072de7e74373457a
Damien Clemenceau <damien.clemenceau@outlook.com>
2017-03-19 11:02:14 +0100

    Integrate game over & credit movie

---
+++ Assets/Scripts/MovieManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieManager : MonoBehaviour {
+    private MovieTexture movieText;
+
+	void Start () {
+        movieText = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
+        movieText.Play();
+    }
+	
+	void Update () {
+		if(!movieText.isPlaying)
+        {
+            LevelManager.GetInstance().GoToMenu();
+        }
+	}
+}
